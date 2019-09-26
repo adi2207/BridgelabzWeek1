9ecb9c08bb43df8fe78c48b39ff7ed81d66f5717
adi2207 <aditipal96@gmail.com>
2019-09-26 19:05:44 +0530

    FDGHFDH

--- fundooNotesApp/src/app/app.module.ts
+++ fundooNotesApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -12,7 +13,8 @@ import { RegisterComponent } from './register/register.component';
     RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- fundooNotesApp/src/app/login/login.component.ts
+++ fundooNotesApp/src/app/login/login.component.ts
@@ -9,17 +9,27 @@ import { User } from './login.model';
 export class LoginComponent implements OnInit {
 
   @Input('user') userObj:User;
-  constructor() { 
+  user:any;
+  response: any;
+   constructor() { 
 
   }
 
-  ngOnInit() {
-    
-       this.user={
-        email: this.userObj.email;
-        password: this.userObj.password;
-       };
+  ngOnInit() {  
+      //  this.user={
+      //   email: this.userObj.email,
+      //   password: this.userObj.password
+      //  };
+
+
   }
-    
+  submitForm(){
+    // this.http.post('ngfjnfm').subscribe((response)=>{
+    //     this.userObj.email= response.email,
+    //     this.userObj.password=response.password
+    //     this.response=response;
+    //   console.log(this.response);
+    // })
+  }  
 
 }

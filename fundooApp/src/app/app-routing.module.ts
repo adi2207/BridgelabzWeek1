6465f9bd3d65d016972cd05f1712735fa35a137f
adi2207 <aditipal96@gmail.com>
2019-09-28 18:19:43 +0530

    DG

--- fundooApp/src/app/app-routing.module.ts
+++ fundooApp/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ForgotComponent } from './forgot/forgot.component';
+import { ResetComponent } from './reset/reset.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'',redirectTo:'/login',pathMatch:'full'},
+  {path:'register',component:RegisterComponent},
+  {path:'forgot',component:ForgotComponent},
+  {path:'reset',component:ResetComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fundooApp/src/app/app.module.ts
+++ fundooApp/src/app/app.module.ts
@@ -2,23 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {AppMaterialModule} from './app.material'
 import 'hammerjs';
+import { FlexLayoutModule } from "@angular/flex-layout";
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ForgotComponent } from './forgot/forgot.component';
+import { ResetComponent } from './reset/reset.component';
+
 // import {ErrorComponent} from './error/error.component'
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ForgotComponent,
+    ResetComponent,
     // ErrorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AppMaterialModule
+    AppMaterialModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

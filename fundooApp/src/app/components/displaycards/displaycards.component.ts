b03d0d56b6a6629ab98244388e0a250cd304f533
adi2207 <aditipal96@gmail.com>
2019-10-04 19:10:11 +0530

    sgdds

--- fundooApp/src/app/components/displaycards/displaycards.component.ts
+++ fundooApp/src/app/components/displaycards/displaycards.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {DataService} from '../../services/data.services/data.service';
 import {NotesService} from '../../services/notes.services/notes.service';
 
 @Component({
